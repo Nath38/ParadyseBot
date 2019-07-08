feb5e80dcbba54475b3afc11becec3a86e574cc6
Nath38 <49879076+Nath38@users.noreply.github.com>
2019-07-08 21:43:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -362,29 +362,4 @@ client.on('message', async message => {
   }
 });
 
-client.on('message', message => {
-
-  if (message.content.startsWith(prefix + 'play')) {
-    let voiceChannel = message.guild.channels
-      .filter(function (channel) { return channel.type === 'voice' })
-      .first()
-    let args = message.content.split(' ')
-    voiceChannel
-      .join()
-      .then(function (connection) {
-        let stream = YoutubeStream(args[1])
-        stream.on('error', function () {
-          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-          connection.disconnect()
-        })
-        connection
-          .playStream(stream)
-          .on('end', function () {
-            connection.disconnect()
-          })
-      })
-  }
-
-})
-
 client.login(process.env.TOKEN);
